a167311ef2073981569585038b084158b51cf5be
Milad Khalighi <milad.kh4lighi@gmail.com>
2022-09-14 01:02:55 -0700

    ready made ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,18 +17,45 @@ class MyApp extends StatelessWidget {
       title: appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: primaryColor,
-          secondary: secondaryColor,
-        ),
+            primary: primaryColor,
+            secondary: secondaryColor,
+            brightness: Brightness.light),
+        scaffoldBackgroundColor: backgroundColor,
         primarySwatch: Colors.deepPurple,
         fontFamily: 'IranSans',
-        textTheme: const TextTheme(
-
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 32,
+            color: primaryTextColor,
+          ),
+          headline2: TextStyle(
+              fontSize: 24,
+              color: primaryTextColor,
+              fontWeight: FontWeight.bold),
+          headline3: TextStyle(
+              fontSize: 16,
+              color: primaryTextColor,
+              fontWeight: FontWeight.bold),
+          bodyText1: TextStyle(
+            fontSize: 16,
+            color: secondaryTextColor,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 14,
+            color: secondaryTextColor,
+          ),
+          button: const TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(style: ElevatedButton.styleFrom(
+          primary: primaryColor,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(1000)),
+          elevation: 16
+        ))
       ),
       home: const Root(),
     );
   }
 }
-
-
